74749c45b84f42f40f9516a610666b2a83da6023
fajp <56133201+fajp@users.noreply.github.com>
2021-06-19 19:57:50 +0200

    v2.0
    Dodane funkcje:
    - komenda xchg dla wszystkich dostępnych rejestrów

--- Program.cs
+++ Program.cs
@@ -112,6 +112,33 @@ namespace emu8086
                 exit = Convert.ToInt32(temp, 16);
             }
         }
+        public static void XCHG(ref int exit, ref int input, string mode)
+        {
+            if (mode.Equals("xx", StringComparison.OrdinalIgnoreCase))
+            {
+                int temp = input;
+                input = exit;
+                exit = temp;
+            }
+            else if (mode.Equals("hh", StringComparison.OrdinalIgnoreCase))
+            {
+                int temp = input;
+                Mov(ref input, exit, "hh");
+                Mov(ref exit, temp, "hh");
+            }
+            else if (mode.Equals("hl", StringComparison.OrdinalIgnoreCase))
+            {
+                int temp = input;
+                Mov(ref input, exit, "lh");
+                Mov(ref exit, temp, "hl");
+            }
+            else if (mode.Equals("ll", StringComparison.OrdinalIgnoreCase))
+            {
+                int temp = input;
+                Mov(ref input, exit, "ll");
+                Mov(ref exit, temp, "ll");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Symulator Intel 8086\n" +
@@ -126,7 +153,7 @@ namespace emu8086
             //Pętla programu
             do
             {
-                
+
                 input = Console.ReadLine(); //Pobieranie komendy od użytkownika
                 string[] Input = input.Split(' '); //Rozdzielenie pobranego stringa na tabelę substringów oddzielonych spacją
 
@@ -205,7 +232,7 @@ namespace emu8086
                     }
                 }
                 else if (Input.Length == 3 && Input[0].Equals("mov", StringComparison.OrdinalIgnoreCase))
-                    //Komeda mov dla pełnych rejestrów
+                //Komeda mov dla pełnych rejestrów
                 {
                     if (Input[1].Equals("ax", StringComparison.OrdinalIgnoreCase)) //Kopiowanie wartości do AX
                     {
@@ -645,6 +672,251 @@ namespace emu8086
                         }
                     }
 
+                }
+                else if (Input.Length == 3 && Input[0].Equals("xchg", StringComparison.OrdinalIgnoreCase))
+                {
+                    //XCHG dla AX i BX
+                    if ((Input[1].Equals("ax", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bx", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("bx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ax", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref bx, "xx");
+                        Console.WriteLine();
+                        /*if (Input[2].Equals("bx", StringComparison.OrdinalIgnoreCase))
+                        {
+                            XCHG(ref ax, ref bx, "xx");
+                            Console.WriteLine();
+                        }
+                        else if (Input[2].Equals("cx", StringComparison.OrdinalIgnoreCase))
+                        {
+                            XCHG(ref ax, ref cx, "xx");
+                            Console.WriteLine();
+                        }
+                        else if (Input[2].Equals("dx", StringComparison.OrdinalIgnoreCase))
+                        {
+                            XCHG(ref ax, ref dx, "xx");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nieprawidłowy rejestr!");
+                            Console.WriteLine();
+                        }*/
+                    }
+                    //XCHG dla AX i CX
+                    else if ((Input[1].Equals("ax", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cx", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ax", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref cx, "xx");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AX i DX
+                    else if ((Input[1].Equals("ax", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dx", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ax", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref dx, "xx");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BX i CX
+                    else if ((Input[1].Equals("bx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cx", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bx", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref cx, "xx");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BX i DX
+                    else if ((Input[1].Equals("bx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dx", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bx", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref dx, "xx");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla CX i DX
+                    else if ((Input[1].Equals("cx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dx", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dx", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cx", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref cx, ref dx, "xx");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i AL
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref ax, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i BH
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref bx, "hh");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i BL
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("bl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref bx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i CH
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref cx, "hh");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i CL
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref cx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i DH
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref dx, "hh");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AH i DL
+                    else if ((Input[1].Equals("ah", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ah", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref dx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AL i BH
+                    else if ((Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref ax, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AL i BL
+                    else if ((Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("bl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref bx, "ll");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AL i CH
+                    else if ((Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref cx, ref ax, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AL i CL
+                    else if ((Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref cx, "ll");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AL i DH
+                    else if ((Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref dx, ref ax, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla AL i DL
+                    else if ((Input[1].Equals("al", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("al", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref ax, ref dx, "ll");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BH i BL
+                    else if ((Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("bl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref bx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BH i CH
+                    else if ((Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref cx, "hh");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BH i CL
+                    else if ((Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref cx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BH i DH
+                    else if ((Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref dx, "hh");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BH i DL
+                    else if ((Input[1].Equals("bh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bh", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref dx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BL i CH
+                    else if ((Input[1].Equals("bl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bl", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref cx, ref bx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BL i CL
+                    else if ((Input[1].Equals("bl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bl", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref bx, ref cx, "ll");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla BL i DH
+                    else if ((Input[1].Equals("bl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("bl", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref dx, ref bx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla CH i CL
+                    else if ((Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("cl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("cl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref cx, ref cx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla CH i DH
+                    else if ((Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dh", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref cx, ref dx, "hh");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla CH i DL
+                    else if ((Input[1].Equals("ch", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("ch", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref cx, ref dx, "hl");
+                        Console.WriteLine();
+                    }
+                    //XCHG dla DH i DL
+                    else if ((Input[1].Equals("dh", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dl", StringComparison.OrdinalIgnoreCase))
+                        || (Input[1].Equals("dl", StringComparison.OrdinalIgnoreCase) && Input[2].Equals("dh", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        XCHG(ref dx, ref dx, "hl");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nieprawidłowy rejestr!");
+                        Console.WriteLine();
+                    }
                 }
                 else if (input.Equals("q", StringComparison.OrdinalIgnoreCase) == false) //Nieznana komenda
                 {
